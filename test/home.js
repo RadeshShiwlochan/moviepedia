793d7f37131bab1381f304ef639266682f3fe6fa
radeshshiwlochan <radesh0430@gmail.com>
2018-10-08 20:13:52 -0400

    adds unit test for home page

---
+++ test/home.js
@@ -0,0 +1,19 @@
+/* 
+    Unit Tests for homepage
+ */
+'use strict';
+
+const app = require('../app.js'); 
+const chai = require('chai');
+const expect = require('chai').expect;
+chai.use(require('chai-http'));
+
+describe('API endpoint /home', () => {
+  it('should render the homepage', () => {
+    return chai.request(app)
+      .get('/home')
+      .then((res) => {
+        expect(res).to.have.status(200);
+      });
+  });
+});

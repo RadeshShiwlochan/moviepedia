83212dffdcdec7f1482220d244ea8a59d28af609
radeshshiwlochan <radesh0430@gmail.com>
2018-10-27 12:39:50 -0400

    adds a function to return the current month

--- utilities/movie-util.js
+++ utilities/movie-util.js
@@ -1,5 +1,31 @@
 const request = require('request');
 
+const getMonth = (month) => {
+  const fullYr = {
+    'Jan': '01',
+    'Feb': '02',
+    'Mar': '03',
+    'Apr': '04',
+    'May': '05',
+    'Jun': '06',
+    'Jul': '07',
+    'Aug': '08',
+    'Sep': '09',
+    'Oct': '10',
+    'Nov': '11',
+    'Dec': '12'
+  }
+  return fullYr[month];
+}
+
+const calcDatePeriod = () => {
+  const date = new Date();
+  let currntDate = date.substring(4,15);
+  let month = currntDate.substring(0,3);
+  let day = currntDate.substring(8,10);
+  let year = currntDate.substring(11); 
+};
+
 const getMovie = () => {
   const movieData = request(process.env.MY_API_KEY, (err, res, body) => {
     console.log('bodies:', body);
@@ -26,8 +52,10 @@ const getMoviesInTheaters = () => {
 }
 
 module.exports = {
-    getMovie,
-    callOMDBApi,
-    getPopMovies,
-    getMoviesInTheaters
+  getMovie,
+  callOMDBApi,
+  getPopMovies,
+  getMoviesInTheaters
 };
+
+console.log(getMonth('Oct'));
